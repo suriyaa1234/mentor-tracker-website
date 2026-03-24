8fae71bdd73124d21cb631e9e851d5df894e0194
Sundar Raj A <your-email@gmail.com>
2026-03-24 13:46:25 +0530

    Completed project with features

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function saveData() {
-    let student = document.getElementById("student").value;
-    let message = document.getElementById("message").value;
-
+function displayData() {
     let list = document.getElementById("list");
+    list.innerHTML = "";
 
-    let item = document.createElement("li");
-    item.textContent = student + " - " + message;
+    let data = JSON.parse(localStorage.getItem("interactions")) || [];
 
-    list.appendChild(item);
+    data.forEach((item, index) => {
+        let li = document.createElement("li");
+        li.textContent = item.student + " - " + item.message;
+
+        let btn = document.createElement("button");
+        btn.textContent = "Delete";
+        btn.onclick = function () {
+            data.splice(index, 1);
+            localStorage.setItem("interactions", JSON.stringify(data));
+            displayData();
+        };
+
+        li.appendChild(btn);
+        list.appendChild(li);
+    });
+}
+function clearAll() {
+    localStorage.removeItem("interactions");
+    displayData();
 }
